3436f691947a2af7c1aa3cee2a87b7e135a36a99
tuan882612 <tuan882612@gmail.com>
2023-09-19 14:44:20 -0500

    feat: updated unit test and added new ones

--- internal/auth/models_test.go
+++ internal/auth/models_test.go
@@ -7,12 +7,13 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/tuan882612/apiutils/securityutils"
 
 	"project/internal/auth"
 )
 
-func TestLoginInputDeserialize(t *testing.T) {
-	data := `{"email": "test@email.com", "password": "password123"}`
+func Test_LoginInput_Deserialize_MissingParams(t *testing.T) {
+	data := `{"email": "test@email.com", "password": "passwordpassword"}`
 	reader := io.NopCloser(bytes.NewReader([]byte(data)))
 
 	input := &auth.LoginInput{}
@@ -20,12 +21,9 @@ func TestLoginInputDeserialize(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
-	if input.Email != "test@email.com" || input.Password != "password123" {
-		t.Errorf("Unexpected input: %+v", input)
-	}
 }
 
-func TestLoginInputDeserializeError(t *testing.T) {
+func Test_LoginInput_Deserialize_Invalid(t *testing.T) {
 	data := `{"email": "test@email.com", "password": }` // malformed JSON
 	reader := io.NopCloser(bytes.NewReader([]byte(data)))
 
@@ -36,7 +34,7 @@ func TestLoginInputDeserializeError(t *testing.T) {
 	}
 }
 
-func TestLoginInputDeserializeEmpty(t *testing.T) {
+func Test_LoginInput_Deserialize_EmptyParams(t *testing.T) {
 	data := `{"email": "", "password": ""}`
 	reader := io.NopCloser(bytes.NewReader([]byte(data)))
 
@@ -50,8 +48,15 @@ func TestLoginInputDeserializeEmpty(t *testing.T) {
 	}
 }
 
+func Test_LoginInput_Deserialize_Nil(t *testing.T) {
+	input := &auth.LoginInput{}
+	if err := input.Deserialize(nil); err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
 func TestRegisterInputDeserialize(t *testing.T) {
-	data := `{"email": "test@email.com", "name": "Test User", "password": "password123"}`
+	data := `{"email": "test@email.com", "name": "Test User", "password": "passwordpassword123"}`
 	reader := io.NopCloser(bytes.NewReader([]byte(data)))
 
 	input := &auth.RegisterInput{}
@@ -59,7 +64,7 @@ func TestRegisterInputDeserialize(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
-	if input.Email != "test@email.com" || input.Name != "Test User" || input.Password != "password123" {
+	if input.Email != "test@email.com" || input.Name != "Test User" || input.Password != "passwordpassword123" {
 		t.Errorf("Unexpected input: %+v", input)
 	}
 }
@@ -93,7 +98,7 @@ func TestNewRegisterResp(t *testing.T) {
 	input := &auth.RegisterInput{
 		Email:    "test@email.com",
 		Name:     "Test User",
-		Password: "password123",
+		Password: "password123password123",
 	}
 
 	resp, err := auth.NewRegisterResp(input)
@@ -108,14 +113,18 @@ func TestNewRegisterResp(t *testing.T) {
 
 func TestNewResgisterRespValues(t *testing.T) {
 	input := &auth.RegisterInput{
-		Email:    "",
-		Name:     "",
-		Password: "",
+		Email:    "test@gmail.com",
+		Name:     "test",
+		Password: "passwordpassword123",
 	}
 
 	resp, err := auth.NewRegisterResp(input)
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if resp.Email != "test@gmail.com" || resp.Name != "test" {
+		t.Errorf("Unexpected response: %+v", resp)
 	}
 
 	if resp.UserID == uuid.Nil || resp.Registered.Before(time.Now().Add(-time.Minute)) || resp.UserStatus != "active" {
@@ -127,5 +136,42 @@ func TestNewRegisterRespError(t *testing.T) {
 	_, err := auth.NewRegisterResp(nil)
 	if err == nil {
 		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestNewRegisterRespEmtyInput(t *testing.T) {
+	input := &auth.RegisterInput{
+		Email:    "test@gmail.com",
+		Name:     "",
+		Password: "passwordpassword123",
+	}
+
+	_, err := auth.NewRegisterResp(input)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestNewRegisterRespNilInput(t *testing.T) {
+	_, err := auth.NewRegisterResp(nil)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestNewRegisterRespProperHash(t *testing.T) {
+	input := &auth.RegisterInput{
+		Email:    "test@gmail.com",
+		Name:     "testing",
+		Password: "passwordpassword123",
+	}
+
+	resp, err := auth.NewRegisterResp(input)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if err := securityutils.ValidatePassword(resp.Password, input.Password); err != nil {
+		t.Errorf("Unexpected error: %v", err)
 	}
 }
