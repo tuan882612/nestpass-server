9e6596415a7964827dfb87c6f37d4535ad575858
tuan882612 <tuan882612@gmail.com>
2023-09-23 21:50:04 -0500

    feat: added struct to hold dependencies

--- internal/auth/models.go
+++ internal/auth/models.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
 	"time"
 
@@ -11,60 +10,51 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/tuan882612/apiutils/securityutils"
 
+	"project/internal/auth/email"
 	"project/internal/auth/jwt"
 	"project/internal/config"
 )
 
-// This struct that holds the dependencies for the base authentication service.
-type Deps struct {
-	Service   Service // base authentication service
-	JWTManger *jwt.JWTManger
+// Dependencies for the base authentication service.
+type Dependencies struct {
+	Repository   Repository // base auth repository
+	Service      Service    // base auth service
+	JWTManager   *jwt.Manager
+	EmailManager *email.Manager
 }
 
-func NewDependencies(cfg *config.Configuration) (*Deps, error) {
-	if cfg == nil {
-		msg := "nil configuration"
-		log.Error().Str("location", "NewDependencies").Msg(msg)
-		return nil, errors.New(msg)
-	}
-
+// Constructor for creating all dependencies for the base authentication service.
+func NewDependencies(cfg *config.Configuration) (*Dependencies, error) {
 	// initialize sub-dependency repository
 	repo, err := NewRepository(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	// initialize base authentication service
-	service, err := NewService(repo)
+	// initialize dependencies
+	emailManager, err := email.NewManger(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	// initialize JWT manager
-	jwtManager, err := jwt.NewJWTManager(cfg.SignKey, cfg.Duration)
-	if err != nil {
-		return nil, err
-	}
+	service := NewService(repo)
+	jwtManager := jwt.NewManager(cfg)
 
-	return &Deps{
-		Service:   service,
-		JWTManger: jwtManager,
+	return &Dependencies{
+		Repository:   repo,
+		Service:      service,
+		JWTManager:   jwtManager,
+		EmailManager: emailManager,
 	}, nil
 }
 
-// This struct is used to take in request data from the login endpoint.
+// Request data from the login endpoint.
 type LoginInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=16,max=32"`
 }
 
 func (li *LoginInput) Deserialize(data io.ReadCloser) error {
-	if data == nil {
-		msg := "nil data"
-		log.Error().Str("location", "LoginInput.Deserialize").Msg(msg)
-		return errors.New(msg)
-	}
-
 	// deserialize the data
 	if err := json.NewDecoder(data).Decode(&li); err != nil {
 		log.Error().Str("location", "LoginInput.Deserialize").Msg(err.Error())
@@ -80,7 +70,7 @@ func (li *LoginInput) Deserialize(data io.ReadCloser) error {
 	return nil
 }
 
-// This struct is used to take in request data from the register endpoint.
+// Request data from the register endpoint.
 type RegisterInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Name     string `json:"name" validate:"required"`
@@ -88,12 +78,6 @@ type RegisterInput struct {
 }
 
 func (ri *RegisterInput) Deserialize(data io.ReadCloser) error {
-	if data == nil {
-		msg := "nil data"
-		log.Error().Str("location", "LoginInput.Deserialize").Msg(msg)
-		return errors.New(msg)
-	}
-
 	// deserialize the data
 	if err := json.NewDecoder(data).Decode(&ri); err != nil {
 		log.Error().Str("location", "RegisterInput.Deserialize").Msg(err.Error())
@@ -109,7 +93,7 @@ func (ri *RegisterInput) Deserialize(data io.ReadCloser) error {
 	return nil
 }
 
-// This struct is used to convert RegisterInput to RegisterResp and validate the input.
+// DTO for RegisterInput to RegisterResp.
 type RegisterResp struct {
 	UserID uuid.UUID `json:"user_id"`
 	RegisterInput
@@ -117,13 +101,8 @@ type RegisterResp struct {
 	UserStatus string    `json:"user_status"`
 }
 
+// Creates a new RegisterResp from the given input and validates it.
 func NewRegisterResp(input *RegisterInput) (*RegisterResp, error) {
-	if input == nil {
-		msg := "register input is nil"
-		log.Error().Str("location", "NewRegisterResp").Msg(msg)
-		return nil, errors.New(msg)
-	}
-
 	// validate the input
 	if err := validator.New().Struct(input); err != nil {
 		log.Error().Str("location", "NewRegisterResp").Msg(err.Error())
@@ -142,7 +121,7 @@ func NewRegisterResp(input *RegisterInput) (*RegisterResp, error) {
 		UserID:        uuid.New(),
 		RegisterInput: *input,
 		Registered:    time.Now(),
-		UserStatus:    "active",
+		UserStatus:    "nonreg",
 	}
 	res.Password = hashedPsw
 	return res, nil
