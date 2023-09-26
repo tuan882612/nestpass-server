2cc04421ad0a6a6671150b4b762b332fbb0e5afa
tuan882612 <tuan882612@gmail.com>
2023-09-26 03:02:31 -0500

    refactor: udpate method names

--- internal/auth/cache.go
+++ internal/auth/cache.go
@@ -21,20 +21,20 @@ func NewCache(databases *database.DataAccess) *Cache {
 }
 
 // Retrieves the user's twofa data.
-func (r *Cache) GetTwofaCache(ctx context.Context, userID uuid.UUID) (*email.TwofaBody, error) {
+func (r *Cache) GetTwofa(ctx context.Context, userID uuid.UUID) (*email.TwofaBody, error) {
 	data, err := r.cache.Get(userID.String()).Result()
 	if err != nil {
 		if err == redis.Nil {
 			return nil, apiutils.NewErrNotFound("twofa not found")
 		}
 
-		log.Error().Str("location", "GetTwofaCache").Msg(err.Error())
+		log.Error().Str("location", "GetTwofaCache").Msgf("%v: failed to get twofa data: %v", userID, err)
 		return nil, err
 	}
 
 	tfaBody := &email.TwofaBody{}
 	if err := tfaBody.Deserialize(data); err != nil {
-		log.Error().Str("location", "GetTwofaCache").Msg(err.Error())
+		log.Error().Str("location", "GetTwofaCache").Msgf("%v: failed to deserialize twofa data: %v", userID, err)
 		return nil, err
 	}
 
@@ -42,10 +42,10 @@ func (r *Cache) GetTwofaCache(ctx context.Context, userID uuid.UUID) (*email.Two
 }
 
 // Updates the user's twofa data.
-func (r *Cache) UpdateTwofaCache(ctx context.Context, userID uuid.UUID, body *email.TwofaBody) error {
+func (r *Cache) UpdateTwofa(ctx context.Context, userID uuid.UUID, body *email.TwofaBody) error {
 	data, err := body.Serialize()
 	if err != nil {
-		log.Error().Str("location", "UpdateTwofaCache").Msg(err.Error())
+		log.Error().Str("location", "UpdateTwofaCache").Msgf("%v: failed to serialize twofa data: %v", userID, err)
 		return err
 	}
 
@@ -53,7 +53,7 @@ func (r *Cache) UpdateTwofaCache(ctx context.Context, userID uuid.UUID, body *em
 	idStr := userID.String()
 	ttl := r.cache.TTL(idStr).Val()
 	if err := r.cache.Set(idStr, data, ttl).Err(); err != nil {
-		log.Error().Str("location", "UpdateTwofaCache").Msg(err.Error())
+		log.Error().Str("location", "UpdateTwofaCache").Msgf("%v: failed to update twofa data: %v", userID, err)
 		return err
 	}
 
@@ -61,9 +61,9 @@ func (r *Cache) UpdateTwofaCache(ctx context.Context, userID uuid.UUID, body *em
 }
 
 // Deletes the user's twofa data.
-func (r *Cache) DeleteTwofaCache(ctx context.Context, userID uuid.UUID) error {
+func (r *Cache) DeleteTwofa(ctx context.Context, userID uuid.UUID) error {
 	if err := r.cache.Del(uuid.UUID.String(userID)).Err(); err != nil {
-		log.Error().Str("location", "DeleteTwofaCache").Msg(err.Error())
+		log.Error().Str("location", "DeleteTwofaCache").Msgf("%v: failed to delete twofa data: %v", userID, err)
 		return err
 	}
 
