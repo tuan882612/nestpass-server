57ef32a11709f4ac22f38e12ae7b06b160a79d84
tuan882612 <tuan882612@gmail.com>
2023-10-16 18:44:00 -0500

    feat: base implementation of OAuth

--- internal/auth/oauth/handler.go
+++ internal/auth/oauth/handler.go
@@ -3,11 +3,13 @@ package oauth
 import (
 	"net/http"
 	"project/internal/config"
+	"time"
 
+	"github.com/rs/zerolog/log"
 	"github.com/tuan882612/apiutils"
 )
 
-type Handler struct{
+type Handler struct {
 	svc *Service
 }
 
@@ -16,16 +18,42 @@ func NewHandler(cfg *config.Configuration) *Handler {
 }
 
 func (h *Handler) Invoke(w http.ResponseWriter, r *http.Request) {
-	resp := apiutils.NewRes(http.StatusOK, "", nil)
-	resp.SendRes(w)
+	state := h.svc.generateStateOauthCookie()
+	http.SetCookie(w, &http.Cookie{
+		Name:    "oauth_state",
+		Value:   state,
+		Expires: time.Now().Add(10 * time.Minute),
+	})
+
+	redirectURL := h.svc.StartOAuth(r.Context(), state)
+	http.Redirect(w, r, redirectURL, http.StatusFound)
+	log.Info().Msg("Redirected to OAuth provider")
 }
 
 func (h *Handler) Callback(w http.ResponseWriter, r *http.Request) {
-	resp := apiutils.NewRes(http.StatusOK, "", nil)
+	c, err := r.Cookie("oauth_state")
+	if err != nil {
+		resp := apiutils.NewRes(http.StatusBadRequest, "Missing state cookie", nil)
+		resp.SendRes(w)
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+	state := r.URL.Query().Get("state")
+
+	token, err := h.svc.CallbackOAuth(r.Context(), code, state, c.Value)
+	if err != nil {
+		resp := apiutils.NewRes(http.StatusInternalServerError, "Failed to get token", err.Error())
+		resp.SendRes(w)
+		return
+	}
+
+	resp := apiutils.NewRes(http.StatusOK, "Authenticated successfully", token)
 	resp.SendRes(w)
 }
 
 func (h *Handler) Refresh(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement token refresh logic here
 	resp := apiutils.NewRes(http.StatusOK, "", nil)
 	resp.SendRes(w)
 }
--- internal/auth/oauth/service.go
+++ internal/auth/oauth/service.go
@@ -2,21 +2,59 @@ package oauth
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"project/internal/config"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
-type Service struct{
-	cfg *config.OAuthConfig
+type Service struct {
+	cfg         *config.OAuthConfig
+	oauthConfig *oauth2.Config
 }
 
 func NewService(oauthCfg *config.OAuthConfig) *Service {
-	return &Service{cfg: oauthCfg}
+	conf := &oauth2.Config{
+		ClientID:     oauthCfg.ClientID,
+		ClientSecret: oauthCfg.ClientSecret,
+		RedirectURL:  "http://localhost:2001/api/v1/oauth/callback",
+		Scopes:       []string{"openid", "profile", "email"},
+		Endpoint:     google.Endpoint,
+	}
+	return &Service{cfg: oauthCfg, oauthConfig: conf}
 }
 
-func (s *Service) StartOAuth(ctx context.Context) {
-
+// Generates a CSRF state token
+func (s *Service) generateStateOauthCookie() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)
 }
 
-func (s *Service) CallbackOAuth(ctx context.Context) {
+func (s *Service) getOAuthData(ctx context.Context, token *oauth2.Token) {
+	return
+}
 
+func (s *Service) StartOAuth(ctx context.Context, state string) string {
+	return s.oauthConfig.AuthCodeURL(state, oauth2.AccessTypeOffline)
+}
+
+func (s *Service) CallbackOAuth(ctx context.Context, code string, state string, expectedState string) (*oauth2.Token, error) {
+	if state != expectedState {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+
+	token, err := s.oauthConfig.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (s *Service) UserLoginSignup(ctx context.Context, token *oauth2.Token) {
+	return
 }
