3164fe2cdf058a06d2d4a82b17477a271281ff24
tuan882612 <tuan882612@gmail.com>
2023-10-19 17:34:35 -0500

    feat: finished authorization middleware

--- internal/server/middlewares/authorization.go
+++ internal/server/middlewares/authorization.go
@@ -1,13 +1,35 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
 
 	"project/internal/config"
+	"project/pkg/auth"
+
+	"github.com/tuan882612/apiutils"
 )
 
-func Authorization(next http.Handler, cfg *config.Configuration) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
+func Authorization(cfg *config.Configuration) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token, err := auth.GetBearerToken(r)
+			if err != nil {
+				apiutils.HandleHttpErrors(w, err)
+				return
+			}
+
+			claims, err := auth.DecodeToken(token, cfg.SignKey)
+			if err != nil {
+				apiutils.HandleHttpErrors(w, err)
+				return
+			}
+
+			// parse and set user id
+			ctx := context.WithValue(r.Context(), auth.CtxUserID, claims.UserID)
+
+			// call next handler
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
---
+++ pkg/auth/jwt.go
@@ -0,0 +1,85 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+	"github.com/tuan882612/apiutils"
+)
+
+type ctxKey string
+
+const CtxUserID ctxKey = "user_id"
+
+// Claims represents the JWT claims.
+type claims struct {
+	jwt.RegisteredClaims
+	UserID uuid.UUID `json:"user_id"`
+}
+
+// DecodeToken decodes a JWT token and returns the Claims.
+func DecodeToken(token, signKey string) (*claims, error) {
+	// decode token with the given sign key
+	payload, err := jwt.ParseWithClaims(token, &claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(signKey), nil
+	})
+
+	// handle all possible errors from parsing the token
+	if err != nil {
+
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, apiutils.NewErrBadRequest(err.Error())
+		} else if errors.Is(err, jwt.ErrSignatureInvalid) {
+			return nil, apiutils.NewErrUnauthorized(err.Error())
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, apiutils.NewErrUnauthorized(err.Error())
+		}
+
+		log.Error().Str("location", "GetPayload").Msg(err.Error())
+		return nil, err
+	}
+
+	// check if the decoded token is valid claims
+	claims, ok := payload.Claims.(*claims)
+	if !ok {
+		errMsg := "error parsing claims"
+		log.Error().Str("location", "New").Msg(errMsg)
+		return nil, errors.New(errMsg)
+	}
+
+	return claims, nil
+}
+
+// Tries to get JWT token from the authorization header.
+func GetBearerToken(r *http.Request) (string, error) {
+	// get the token from the authorization header
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", apiutils.NewErrUnauthorized("missing authorization header")
+	}
+
+	// tries to parse the token from the authorization header
+	splitToken := strings.Split(authHeader, "Bearer ")
+	if len(splitToken) != 2 || splitToken[1] == "" {
+		return "", apiutils.NewErrUnauthorized("invalid authorization header")
+	}
+
+	return splitToken[1], nil
+}
+
+// Parses and retrieves the user id from the context.
+func UidFromCtx(ctx context.Context) (uuid.UUID, error) {
+	uid, ok := ctx.Value(CtxUserID).(uuid.UUID)
+	if !ok {
+		errMsg := "error parsing user id"
+		log.Error().Str("location", "UidFromCtx").Msg(errMsg)
+		return uuid.Nil, errors.New(errMsg)
+	}
+
+	return uid, nil
+}
