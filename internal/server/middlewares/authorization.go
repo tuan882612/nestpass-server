dbd44c73783bc00ab9824f06d25cd5a4bd9b3a84
tuan882612 <tuan882612@gmail.com>
2023-10-23 12:57:10 -0500

    feat: implemented double hash submit validation

--- internal/server/middlewares/authorization.go
+++ internal/server/middlewares/authorization.go
@@ -4,21 +4,35 @@ import (
 	"context"
 	"net/http"
 
-	"project/internal/config"
-	"project/pkg/auth"
-
 	"github.com/tuan882612/apiutils"
+
+	"nestpass/internal/config"
+	"nestpass/pkg/auth"
 )
 
 func Authorization(cfg *config.Configuration) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// gets the JWT from the authorization header
 			token, err := auth.GetBearerToken(r)
 			if err != nil {
 				apiutils.HandleHttpErrors(w, err)
 				return
 			}
 
+			// double submit cookie verification
+			cookie, err := r.Cookie("session")
+			if err != nil {
+				apiutils.HandleHttpErrors(w, apiutils.NewErrUnauthorized("Missing JWT cookie"))
+				return
+			}
+
+			if token != cookie.Value {
+				apiutils.HandleHttpErrors(w, apiutils.NewErrUnauthorized("CSRF tokens do not match"))
+				return
+			}
+
+			// decode JWT token
 			claims, err := auth.DecodeToken(token, cfg.SignKey)
 			if err != nil {
 				apiutils.HandleHttpErrors(w, err)
